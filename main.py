4cc92ea6dd8b58e3122f2209b4dd066e28571210
MysticIS218 <91106087+MysticIS218@users.noreply.github.com>
2021-10-19 13:08:36 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 """ content of calculator.py#"""
-# disabling:
-# C0304: Trailing newlines (Trailing-newlines)
-# pylint: disable=C0304
+
 
 def inc(x_value):
     """ Increment Function adds one to the x_value"""
-
     return x_value + 1
+
+
 def test_answer():
     """This Tests the function"""
-
     assert inc(4) == 5
-
 
 
