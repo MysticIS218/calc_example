97b013552e70ea5acbf4630d635015069d46c06b
MysticIS218 <91106087+MysticIS218@users.noreply.github.com>
2021-11-20 11:49:11 -0500

    Create division.py

---
+++ calc/calculations/division.py
@@ -0,0 +1,13 @@
+from calc.calculations.calculation import Calculation
+
+class Division(Calculation): # pylint: disable=too-few-public-methods
+    
+    """ calculation addition class"""
+    def get_result(self):
+        """get the addition results"""
+        divide_values = self.values[0]
+        for value in self.values:
+            if value == 0:
+                raise Exception('ZeroDivisionError')
+            divide_values = divide_values / value
+            return divide_values
