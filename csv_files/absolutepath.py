d248e59f8ebf8209a217065a2f67c4c6b08566df
MysticIS218 <91106087+MysticIS218@users.noreply.github.com>
2021-12-05 16:44:10 -0500

    Delete csv_files directory

--- csv_files/absolutepath.py
+++
@@ -1,8 +0,0 @@
-"""Contains the absolute Path"""
-from pathlib import Path
-
-
-def absolute_path(filepath):
-    """gets absolute path from relative path"""
-    relative = Path(filepath)
-    return relative.absolute()
--- csv_files/csv_reader.py
+++
@@ -1,4 +0,0 @@
-import calculator.csv_files.absolute_path as ap
-import pandas as pd
-from datetime import datetime
-from calculator.csv_files.csv_writer import CSV_Writer
