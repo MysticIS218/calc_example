771a28193557f55534df4531ead5e5954ae77a9f
MysticIS218 <91106087+MysticIS218@users.noreply.github.com>
2021-12-07 09:47:09 -0500

    Create main.py

---
+++ calc/main.py
@@ -0,0 +1,11 @@
+"""Reader Main"""
+from calc.csv_files.csv_reader import CSV_Reader
+
+
+def read_csv_content(absPath: str):
+    """get contents"""
+    dummy = get_csv_content.read_csv_content(absPath)
+    return
+
+"""Holds path to parse test data"""
+get_csv_content.csv_addition('../tests/test_data/addition_data.csv')
